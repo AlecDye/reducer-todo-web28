420cc87695f8990470753b5ee05bb4b46e3f08f2
Alec Dye <alecdye@gmail.com>
2020-03-16 12:54:12 -0700

    Building initial state and reducer

---
+++ src/reducers/index.js
@@ -0,0 +1,12 @@
+export const initialState = {
+  item: "Learn about reducers",
+  completed: false,
+  id: 2342341324
+};
+
+export const taskReducer = (state, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
